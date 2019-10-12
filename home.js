b5b613a8d10445c1fabe957c4b662481d24d6887
BhandariM <s525703@mail.nwmissouri.edu>
2019-10-11 22:33:20 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+var test = "mhi";
+console.log(typeof(test));
+
+// function formSubmit(){
+//     Console.log("mounika");
+//     document.getElementById("form").submit();
+// }
+
+function changeComment(){
+    // console.log( document.getElementById("form").submit(),"mounika")
+    // alert("Thanks for your valuable comments");
+    var sub =    document.getElementById("commentbox").nodeValue;
+    console.log(sub, "moi");
+ //  document.getElementById("comment").innerHTML = "Thanks for your valuable comments.";
+    }
+
