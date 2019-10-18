b4a7fec07fdd362548b3e54231aed7a599aac527
BhandariM <s525703@mail.nwmissouri.edu>
2019-10-18 11:11:17 -0400

    Add files via upload

--- home.js
+++ home.js
@@ -12,10 +12,10 @@ function formSubmit(){
     var frstdrpdwn =   document.getElementById("mainMenu").value;
     var scnddrpdwn =   document.getElementById("secondMenu").value;
     var cmnt =   document.getElementById("commentbox").value;
-    console.log(uname,  "mouni uname submit");
-    console.log(frstdrpdwn,  "mouni uname submit");
-    console.log(scnddrpdwn,  "mouni uname submit");
-    console.log(cmnt, "mouni cmnt submit");
+    console.log(uname,  "is my name");
+    console.log(frstdrpdwn,  "is the score");
+    console.log(scnddrpdwn,  "is the rating");
+    console.log(cmnt, "are the comments ");
     var clear =   document.getElementById("username").value = " ";
     var clear =   document.getElementById("mainMenu").value = " ";
     var clear =   document.getElementById("secondMenu").value = " ";
@@ -28,34 +28,58 @@ function formSubmit(){
     
    }
 
-var rating =
-[
-    {
-    "name": "Good",
-    "abbreviation": "gud"
-  },
-  {
-    "name": "Bad",
-    "abbreviation": "bad"
-  },
-  {
-    "name": "Avg",
-    "abbreviation": "avg"
+// var rating =
+// [
+//     {
+//     "name": "Good",
+//     "abbreviation": "gud"
+//   },
+//   {
+//     "name": "Bad",
+//     "abbreviation": "bad"
+//   },
+//   {
+//     "name": "Avg",
+//     "abbreviation": "avg"
+//   }
+// ];
+
+let rating = document.getElementById("mainMenu");
+const url1 = "https://api.myjson.com/bins/7xq2x";
+const request1 = new XMLHttpRequest();
+request1.open('GET', url1, true);
+request1.onload = function(){
+  if(request1. status == 200){
+    const data = JSON.parse(request1.responseText);
+    let option;
+   for(let i=0; i<=2; i++){
+      option = document.createElement('option');
+      option.text = data[i].name;
+      option.value = data[i].abbrevation;
+      mainMenu.add(option);
   }
-];
+  }
+  else{
+  console.error('An error occurred fetching the JSON from ' + url1);
+  }
+}
+
+request1.send();
+
+
 
 
 let secondDropdown = document.getElementById('secondMenu');
 secondDropdown.length = 0;
 
 let defaultOption = document.createElement('option');
-defaultOption.text = 'Choose State/Province';
+defaultOption.text = 'Choose rating';
 
 secondDropdown.add(defaultOption);
 secondDropdown.selectedIndex = 0;
 
-//const url = 'https://jsonplaceholder.typicode.com/comments';
-const url = 'https://api.myjson.com/bins/7xq2x';
+const url = 'https://jsonplaceholder.typicode.com/comments';
+//const url = 'https://api.myjson.com/bins/7xq2x';
 
 const request = new XMLHttpRequest();
 request.open('GET', url, true);
@@ -64,10 +88,10 @@ request.onload = function() {
   if (request.status === 200) {
     const data = JSON.parse(request.responseText);
     let option;
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i <=4; i++) {
       option = document.createElement('option');
-      option.text = data[i].name;
-      option.value = data[i].abbreviation;
+      option.text = data[i].id;
+     // option.value = data[i].id;
       secondDropdown.add(option);
     }
    } else {
