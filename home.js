c2c07f0173081f7af3537c3e8efe1e9a3616941b
BhandariM <s525703@mail.nwmissouri.edu>
2019-10-21 01:01:05 -0400

    Add files via upload

--- home.js
+++ home.js
@@ -1,17 +1,41 @@
-var test = "mhi";
-console.log(typeof(test));
 
-// function formSubmit(){
-//     Console.log("mounika");
-//     document.getElementById("form").submit();
-// }
+
 
 function formSubmit(){
-    // console.log( document.getElementById("form").submit(),"mounika")
-    var uname =  document.getElementById("username").value;
-    var frstdrpdwn =   document.getElementById("mainMenu").value;
-    var scnddrpdwn =   document.getElementById("secondMenu").value;
-    var cmnt =   document.getElementById("commentbox").value;
+var uname =  document.getElementById("username").value;
+var frstdrpdwn = document.getElementById("mainMenu").value;
+var scnddrpdwn = document.getElementById("secondMenu").value;
+var cmnt = document.getElementById("commentbox").value;
+//----form validation-----
+// if(uname || frstdrpdwn||scnddrpdwn||cmnt == ""){
+//   alert( "Please enter all fields!" );
+//   return false;
+// }
+if(frstdrpdwn.value == "good"){
+  document.getElementById("secondMenu").disable=false;
+}
+  else{
+    document.getElementById("secondMenu").disable=true;
+  }
+
+
+// if(uname  == ""){
+//   alert( "Please provide your name!" );
+//   return false;
+// }
+// if(frstdrpdwn == ""){
+//   alert( "Please describe the website!" );
+//   return false;
+// }
+// if(scnddrpdwn == ""){
+//   alert( "Please provide your rating!" );
+//   return false;
+// }
+// if(cmnt == ""){
+//   alert( "Please provide your comments!" );
+//   return false;
+// }
+
     console.log(uname,  "is my name");
     console.log(frstdrpdwn,  "is the score");
     console.log(scnddrpdwn,  "is the rating");
@@ -24,9 +48,10 @@ function formSubmit(){
     document.getElementById("comment").innerHTML == "If you like or dislike this page please enter comments or suggestions below."
     + '\n' + " This helps me to improve my skills in required areas. Thanks!.";
     console.log(clear, "mouni clear");
-
     
    }
+
+ //  var local_data = data;
 
 // var rating =
 // [
@@ -43,47 +68,45 @@ function formSubmit(){
 //     "abbreviation": "avg"
 //   }
 // ];
+//------api call for first dropdown-----
+// let rating = document.getElementById("mainMenu");
+// mainMenu.length = 0;
+// let defaOption = document.createElement('option');
+// defaOption.text = 'Choose score';
+// mainMenu.add(defaOption);
+// mainMenu.selectedIndex = 0;
+// const url1 = "https://api.myjson.com/bins/7xq2x";
+// const request1 = new XMLHttpRequest();
+// request1.open('GET', url1, true);
+// request1.onload = function(){
+//   if(request1. status == 200){
+//     const data = JSON.parse(request1.responseText);
+//     let option;
+//    for(let i=0; i<=2; i++){
+//       option = document.createElement('option');
+//       option.text = data[i].name;
+//     //  option.value = data[i].abbrevation;
+//       mainMenu.add(option);
+//   }
+//   }
+//   else{
+//   console.error('An error occurred fetching the JSON from ' + url1);
+//   }
+// }
+// request1.send();
 
-let rating = document.getElementById("mainMenu");
-const url1 = "https://api.myjson.com/bins/7xq2x";
-const request1 = new XMLHttpRequest();
-request1.open('GET', url1, true);
-request1.onload = function(){
-  if(request1. status == 200){
-    const data = JSON.parse(request1.responseText);
-    let option;
-   for(let i=0; i<=2; i++){
-      option = document.createElement('option');
-      option.text = data[i].name;
-      option.value = data[i].abbrevation;
-      mainMenu.add(option);
-  }
-  }
-  else{
-  console.error('An error occurred fetching the JSON from ' + url1);
-  }
-}
 
-request1.send();
-
-
-
-
+//-----api call for the second dropdown----
 let secondDropdown = document.getElementById('secondMenu');
 secondDropdown.length = 0;
-
 let defaultOption = document.createElement('option');
 defaultOption.text = 'Choose rating';
-
 secondDropdown.add(defaultOption);
 secondDropdown.selectedIndex = 0;
-
 const url = 'https://jsonplaceholder.typicode.com/comments';
 //const url = 'https://api.myjson.com/bins/7xq2x';
-
 const request = new XMLHttpRequest();
 request.open('GET', url, true);
-
 request.onload = function() {
   if (request.status === 200) {
     const data = JSON.parse(request.responseText);
@@ -98,9 +121,10 @@ request.onload = function() {
     // Reached the server, but it returned an error
   }   
 }
-
 request.onerror = function() {
   console.error('An error occurred fetching the JSON from ' + url);
 };
-
 request.send();
+
+
+
