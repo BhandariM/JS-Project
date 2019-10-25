f57c627f3a5b37b1baf690fff60d8c6c5b4987d1
BhandariM <s525703@mail.nwmissouri.edu>
2019-10-25 15:19:32 -0400

    Add files via upload

--- home.js
+++ home.js
@@ -1,57 +1,61 @@
+function formSubmit() {
+  // var uname = document.getElementById("username").value;
+  // var frstdrpdwn = document.getElementById("mainMenu").value;
+  // var scnddrpdwn = document.getElementById("secondMenu").value;
+  // var cmnt = document.getElementById("commentbox").value;
+  // var inputs = document.getElementsByClassName('validate');
 
-
-
-function formSubmit(){
-var uname =  document.getElementById("username").value;
-var frstdrpdwn = document.getElementById("mainMenu").value;
-var scnddrpdwn = document.getElementById("secondMenu").value;
-var cmnt = document.getElementById("commentbox").value;
-//----form validation-----
-// if(uname || frstdrpdwn||scnddrpdwn||cmnt == ""){
-//   alert( "Please enter all fields!" );
-//   return false;
-// }
-if(frstdrpdwn.value == "good"){
-  document.getElementById("secondMenu").disable=false;
-}
-  else{
-    document.getElementById("secondMenu").disable=true;
+  for (var i = 0; i < inputs.length; i++) {
+    var element = inputs[i];
+    if (!element.value) {
+      alert("Please enter all fields!");
+      return false;
+    }
   }
+  console.log(uname, "is my name");
+  console.log(frstdrpdwn, "is the score");
+  console.log(scnddrpdwn, "is the rating");
+  console.log(cmnt, "are the comments ");
+
+  for (var j = 0; j < inputs.length; j++) {
+    var elementClear = inputs[j];
+    elementClear.value = "";
+  }
+  //----form validation-----
 
 
-// if(uname  == ""){
-//   alert( "Please provide your name!" );
-//   return false;
-// }
-// if(frstdrpdwn == ""){
-//   alert( "Please describe the website!" );
-//   return false;
-// }
-// if(scnddrpdwn == ""){
-//   alert( "Please provide your rating!" );
-//   return false;
-// }
-// if(cmnt == ""){
-//   alert( "Please provide your comments!" );
-//   return false;
-// }
+  // if (frstdrpdwn.value == "good") {
+  //   document.getElementById("secondMenu").disable = false;
+  // } else {
+  //   document.getElementById("secondMenu").disable = true;
+  // }
 
-    console.log(uname,  "is my name");
-    console.log(frstdrpdwn,  "is the score");
-    console.log(scnddrpdwn,  "is the rating");
-    console.log(cmnt, "are the comments ");
-    var clear =   document.getElementById("username").value = " ";
-    var clear =   document.getElementById("mainMenu").value = " ";
-    var clear =   document.getElementById("secondMenu").value = " ";
-    var clear =   document.getElementById("commentbox").value = " ";
-    alert("Thanks for your valuable input");
-    document.getElementById("comment").innerHTML == "If you like or dislike this page please enter comments or suggestions below."
-    + '\n' + " This helps me to improve my skills in required areas. Thanks!.";
-    console.log(clear, "mouni clear");
-    
-   }
 
- //  var local_data = data;
+  // if(uname  == ""){
+  //   alert( "Please provide your name!" );
+  //   return false;
+  // }
+  // if(frstdrpdwn == ""){
+  //   alert( "Please describe the website!" );
+  //   return false;
+  // }
+  // if(scnddrpdwn == ""){
+  //   alert( "Please provide your rating!" );
+  //   return false;
+  // }
+  // if(cmnt == ""){
+  //   alert( "Please provide your comments!" );
+  //   return false;
+  // }
+
+ 
+  alert("Thanks for your valuable input");
+  document.getElementById("comment").innerHTML == "If you like or dislike this page please enter comments or suggestions below." +
+    '\n' + " This helps me to improve my skills in required areas. Thanks!.";
+
+}
+
+//  var local_data = data;
 
 // var rating =
 // [
@@ -107,24 +111,21 @@ const url = 'https://jsonplaceholder.typicode.com/comments';
 //const url = 'https://api.myjson.com/bins/7xq2x';
 const request = new XMLHttpRequest();
 request.open('GET', url, true);
-request.onload = function() {
+request.onload = function () {
   if (request.status === 200) {
     const data = JSON.parse(request.responseText);
     let option;
-    for (let i = 0; i <=4; i++) {
+    for (let i = 0; i <= 4; i++) {
       option = document.createElement('option');
       option.text = data[i].id;
-     // option.value = data[i].id;
+      // option.value = data[i].id;
       secondDropdown.add(option);
     }
-   } else {
+  } else {
     // Reached the server, but it returned an error
-  }   
+  }
 }
-request.onerror = function() {
+request.onerror = function () {
   console.error('An error occurred fetching the JSON from ' + url);
 };
 request.send();
-
-
-
