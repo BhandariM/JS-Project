e5fe02104a0cde80f8223beff5f397939b5393cb
BhandariM <s525703@mail.nwmissouri.edu>
2019-11-14 22:53:16 -0500

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,3 @@
+function redirect() { 
+    window.location.href("https://www.linkedin.com/in/mounika-banda-290b7b151/"); 
+}
--- home.js
+++ home.js
@@ -1,3 +1,6 @@
+
+
+
 function formSubmit() {
   var uname = document.getElementById("username").value;
   var frstdrpdwn = document.getElementById("mainMenu").value;
@@ -14,12 +17,12 @@ function formSubmit() {
   if (uname == '' || frstdrpdwn == '' || scnddrpdwn == '' || phnum == '' || cmnt == '') {
     alert("Please enter all fields!");
     return false;
-  }
+  } 
   // else {
   //   document.getElementById("subbtn").disabled = true;
   // }
 
-
+  
 
   console.log(uname, "is my name");
   console.log(frstdrpdwn, "is the score");
@@ -38,6 +41,13 @@ function formSubmit() {
 
 }
 
+//--phone number validation
+// function validatePhoneNumber(elementValue){
+//   var phoneNumberPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+//   return phoneNumberPattern.test(elementValue);
+//   }
+
+
 function inputOnblue(e) {
   console.log(e);
   if (isNaN(Number(e.target.value))) {
@@ -45,20 +55,33 @@ function inputOnblue(e) {
   }
 }
 
-function phnumber(e) {
-  console.log('hahahah');
+// function phnumber(e) {
+//   console.log('hahahah');
+//   // var phoneno = /^\d{10}$/;
+//   var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+//   //var inputtxt = e.target.value;
+//   if ( e.target.value.match(phoneno)) {
+//     return true;
+//   } else {
+//     alert("message");
+//     return false;
+//   }
+//}
 
-  // var phoneno = /^\d{10}$/;
-  var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-  var inputtxt = e.target.value;
-  if (inputtxt.match(phoneno)) {
-    return true;
-  } else {
-    alert("message");
-    return false;
+
+  function phnum(inputtxt){
+       var ph = /^\+1?\d{0-9}$/;
+//    var phoneno = /^\d{10}$/;
+//   // var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+   if((inputtxt.value.match(phoneno))) 
+   {
+       return true;
+      }
+       else{
+         alert("message");
+         return false;
+         }
   }
-}
-
 
 //------api call for first dropdown from local json file-----
 let rating = document.getElementById("mainMenu");
@@ -68,21 +91,22 @@ defaOption.text = 'Choose score';
 mainMenu.add(defaOption);
 mainMenu.selectedIndex = 0;
 // const url1 = "https://api.myjson.com/bins/7xq2x";
-const url1 = "home.json";
+ const url1 = "home.json";
 const request1 = new XMLHttpRequest();
 request1.open('GET', url1, true);
-request1.onload = function () {
-  if (request1.status == 200) {
+request1.onload = function(){
+  if(request1. status == 200){
     const data = JSON.parse(request1.responseText);
     let option;
-    for (let i = 0; i <= 2; i++) {
+   for(let i=0; i<=2; i++){
       option = document.createElement('option');
       option.text = data[i].name;
-      //  option.value = data[i].abbrevation;
+    //  option.value = data[i].abbrevation;
       mainMenu.add(option);
-    }
-  } else {
-    console.error('An error occurred fetching the JSON from ' + url1);
+  }
+  }
+  else{
+  console.error('An error occurred fetching the JSON from ' + url1);
   }
 }
 request1.send();
@@ -122,7 +146,7 @@ request1.send();
 // function populateRatings(items = []) {
 //   var length = secondDropdown.options.length;
 //   console.log(secondDropdown.options.length);
-
+  
 //   for (i = 0; i < length; i++) {
 //     secondDropdown.options[i] = null;
 //   }
@@ -147,38 +171,37 @@ request1.send();
 //     mainMenu.add(option);
 // }
 
-function dynamicdropdown(listindex) {
-  //   if(mainMenu.value= "good"){
-  //     populateRatings([1,2]);
+function dynamicdropdown(listindex)
+{
+//   if(mainMenu.value= "good"){
+//     populateRatings([1,2]);
 
-  //   }
-  //   if(mainMenu.value= "avg"){
-  //     populateRatings([3,4]);
+//   }
+//   if(mainMenu.value= "avg"){
+//     populateRatings([3,4]);
 
-  //   }
-  //   if(mainMenu.value= "bad"){
-  //     populateRatings([5]);
+//   }
+//   if(mainMenu.value= "bad"){
+//     populateRatings([5]);
 
-  //   }
-  // }
-
-
-
-  switch (listindex) {
-    case "GOOD":
-      document.getElementById("secondMenu").options[0] = new Option("Choose Rating", "");
-      document.getElementById("secondMenu").options[1] = new Option("1", "1");
-      document.getElementById("secondMenu").options[2] = new Option("2", "2");
-      break;
-    case "AVG":
-      document.getElementById("secondMenu").options[0] = new Option("Choose Rating", "");
-      document.getElementById("secondMenu").options[1] = new Option("3", "3");
-      document.getElementById("secondMenu").options[2] = new Option("4", "4");
-      break;
-    case "BAD":
-      document.getElementById("secondMenu").options[0] = new Option("Choose Rating", "");
-      document.getElementById("secondMenu").options[1] = new Option("5", "5");
-      break;
-  }
-  return true;
-}
+//   }
+// }
+    switch (listindex)
+    {
+    case "GOOD" :
+        document.getElementById("secondMenu").options[0]=new Option("Choose Rating","");
+        document.getElementById("secondMenu").options[1]=new Option("1","1");
+        document.getElementById("secondMenu").options[2]=new Option("2","2");
+        break;
+    case "AVG" :
+        document.getElementById("secondMenu").options[0]=new Option("Choose Rating","");
+        document.getElementById("secondMenu").options[1]=new Option("3","3");
+        document.getElementById("secondMenu").options[2]=new Option("4","4");
+        break;
+    case "BAD" :
+          document.getElementById("secondMenu").options[0]=new Option("Choose Rating","");
+          document.getElementById("secondMenu").options[1]=new Option("5","5");
+          break;
+    }
+    return true;
+    }
